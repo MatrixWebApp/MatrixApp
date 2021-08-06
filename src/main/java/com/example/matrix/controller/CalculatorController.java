d640ab71ce72d7dbab5888becaa6af734bf4c1cd
damirJann <pilacis@list.ru>
2021-08-06 18:37:53 +0300

    add ability to response to cors request

--- src/main/java/com/example/matrix/controller/CalculatorController.java
+++ src/main/java/com/example/matrix/controller/CalculatorController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ import java.util.Map;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CalculatorController {
     @Autowired
     CalculatorService calculatorService;
